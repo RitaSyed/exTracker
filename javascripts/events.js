99d5076ccdff95a4f257979e942612e2a9ecc6b3
Rita Patrick <rita.patrick1007@gmail.com>
2018-05-05 10:37:56 -0500

    case insensitive

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,9 +1,15 @@
 
+jQuery.expr[':'].icontains = function (a, i, m) {
+  return jQuery(a).text().toUpperCase()
+    .indexOf(m[3].toUpperCase()) >= 0;
+};
+
 const searchCards = (e) => {
   const input = $('.input-box').val();
   console.log(input);
   if (e.which === 13) {
-    console.log($(`.thumbnail:not(:contains(${input}))`).hide());
+    console.log($(`.thumbnail:not(:icontains(${input}))`).hide());
+    // console.log($(`.thumbnail:not(:contains.toLowerCase()(${input}))`));
     // function () {
     // $(this).toggle($(this).text().toLowerCase().indexOf(input) > -1);
     // });
